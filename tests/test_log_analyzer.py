deab5c14026ace8751e5af81467b286bf0834276
mlukashov <menguk@mail.ru>
2024-03-17 12:24:57 +0300

    correct tests

--- tests/test_log_analyzer.py
+++ tests/test_log_analyzer.py
@@ -1,4 +1,4 @@
-from log_analyzer import parse_log, generate_report
+from .. log_analyzer  import parse_log, generate_report
 
 def test_parse_log():
     test_log_file = 'test_log.txt'
@@ -7,7 +7,8 @@ def test_parse_log():
     log_data = parse_log(test_log_file)
     assert '/api/v2/banner/16852664' in log_data
 
-def test_generate_report ():
+
+def test_generate_report():
     # Подготавливаем тестовые данные для отчета
     log_data = {'url1': [1.2, 3.4], 'url2': [5.6, 7.8]}
     report_size = 2
@@ -15,7 +16,3 @@ def test_generate_report ():
     # Проверяем, что функция generate_report корректно генерирует отчет
     report = generate_report(log_data, report_size)
     assert 'url1' in report
-
-
-
-
